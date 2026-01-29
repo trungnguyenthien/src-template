f78d48c3ca6bf59f4f092a533888d84feb2e340f
TrungNguyen <>
2026-01-30 06:16:24 +0700

    feat: Add JetBrains Compose and Compose Compiler plugins to build.gradle.kts

--- cmpModule/build.gradle.kts
+++ cmpModule/build.gradle.kts
@@ -2,4 +2,6 @@ plugins {
     alias(libs.plugins.android.kotlin.multiplatform.library) apply false
     alias(libs.plugins.kotlinMultiplatform) apply  false
     alias(libs.plugins.vanniktech.mavenPublish) apply false
+    alias(libs.plugins.jetbrainsCompose) apply false
+    alias(libs.plugins.compose.compiler) apply false
 }
--- cmpModule/library/build.gradle.kts
+++ cmpModule/library/build.gradle.kts
@@ -3,18 +3,20 @@ import org.jetbrains.kotlin.gradle.dsl.JvmTarget
 
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
+    alias(libs.plugins.jetbrainsCompose)
+    alias(libs.plugins.compose.compiler)
     alias(libs.plugins.android.kotlin.multiplatform.library)
     alias(libs.plugins.vanniktech.mavenPublish)
 }
 
 group = "io.github.kotlin"
 version = "1.0.0"
-artifactId = "library"
-androidLibraryNamespace = "org.jetbrains.kotlinx.multiplatform.library.template"
+val artifactId = "library"
+val androidLibraryNamespace = "org.jetbrains.kotlinx.multiplatform.library.template"
 
 kotlin {
     androidLibrary {
-        namespace = androidLibraryNamespace
+        namespace = androidLibraryNamespace.toString()
         compileSdk = libs.versions.android.compileSdk.get().toInt()
         minSdk = libs.versions.android.minSdk.get().toInt()
 
